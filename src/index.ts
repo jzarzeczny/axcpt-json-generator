ae838abde8260f8d986d10fa5eabfc01c574212f
jzarzeczny <jkzarzeczny@gmail.com>
2022-02-14 19:05:19 +0100

    removing O and 0

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,6 @@ const POSSIBLE_LETTERS: string[] = [
   "L",
   "M",
   "N",
-  "O",
   "P",
   "Q",
   "R",
@@ -46,7 +45,7 @@ const POSSIBLE_LETTERS: string[] = [
 
 const AFFECT_ID: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-const NUMBERS: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const NUMBERS: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const A: string = "A";
 const X: string = "X";
